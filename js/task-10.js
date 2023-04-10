7f695ff81bc5e49070dce0d60989caf250e19867
ylin14 <108520955+ylin14@users.noreply.github.com>
2023-04-10 19:44:25 +0300

    fixed issues

--- js/task-10.js
+++ js/task-10.js
@@ -1,25 +1,54 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-
 const ref = {
   divBoxesEl: document.querySelector("#boxes"),
   inputEl: document.querySelector("input"),
   createBtnEl: document.querySelector("[data-create]"),
   destroyBtn: document.querySelector("[data-destroy]"),
-  newDivka: document.createElement("div"),
+};
+
+const create = {
+  createDiv: function () {
+    const newDivka = document.createElement("div");
+    newDivka.style.width = '30px';
+    newDivka.style.height = '30px';
+    return newDivka;
+  },
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 };
 
 const createBoxes = (amount) => {
   const newEl = [];
+
   for (let i = 0; i < amount; i += 1) {
-    newEl.push(ref.newDivka);
-  }
+    newEl.push(create.createDiv());
+    newEl[i].style.backgroundColor = getRandomHexColor();
+    console.log(newEl[i]);
+  };
+
+  for (let i = 1; i < amount; i += 1) {
+  newEl[i].style.width = `${30 + i * 10}px`;
+  newEl[i].style.height = `${30 + i * 10}px`;
+  };
+
   return newEl;
 };
 
-console.log(createBoxes(3));
+function onCreateButton () {
+  return ref.divBoxesEl.append(...createBoxes(ref.inputEl.value));
+};
 
-ref.divBoxesEl.append(...createBoxes(3));
+function onDestroyButton () {
+  return ref.divBoxesEl.querySelectorAll('div').forEach((el) => {
+    return el.remove();
+  });
+};
+
+ref.createBtnEl.addEventListener('click', onCreateButton);
+ref.destroyBtn.addEventListener('click', onDestroyButton);
+
+
+
